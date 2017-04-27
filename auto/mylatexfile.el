6057f45d703118a724bc167c5d3be30d2b091116
chetnashah <chetshah80@gmail.com>
2017-04-27 21:54:15 +0530

    first commit

--- auto/mylatexfile.el
+++ auto/mylatexfile.el
@@ -1,13 +1,22 @@
 (TeX-add-style-hook
  "mylatexfile"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("eulervm" "euler-digits") ("ulem" "normalem")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art10"
+    "eulervm"
     "lipsum"
     "mathtools"
     "amsthm"
-    "amssymb"))
+    "amssymb"
+    "ulem")
+   (TeX-add-symbols
+    '("fallingfactorial" 1))
+   (LaTeX-add-mathtools-DeclarePairedDelimiters
+    '("ceil" "")
+    '("floor" "")))
  :latex)
 
